b1006cc1b7847cd3904f34e3b42f16855e3043b3
ajay9933 <ajay.alla@zemosolabs.com>
2024-10-08 17:19:07 +0530

    Fix: Bookservice

--- book-service/src/main/java/com/ajay/book_service/service/BookServiceImpl.java
+++ book-service/src/main/java/com/ajay/book_service/service/BookServiceImpl.java
@@ -17,7 +17,7 @@ public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
 
-    @Autowired
+   
     public BookServiceImpl(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
